a82fcb8cfcdd0389d8670ac1329e1693e8c95a95
Filipe-uergs <filipe-mengue@uergs.edu.br>
2025-11-09 15:29:10 -0300

    Versão simples de servidor

--- Servidor.js
+++ Servidor.js
@@ -6,6 +6,7 @@ const { Server } = require("socket.io");
 // Cria app e servidor
 const app = express();
 const server = http.createServer(app);
+const rooms = {}; // Armazena as salas e seus jogadores
 const io = new Server(server);
 
 // Servir arquivos estáticos da pasta "public" (cliente)
@@ -14,6 +15,36 @@ app.use(express.static("public"));
 // Quando um cliente se conecta
 io.on("connection", (socket) => {
   console.log("Novo jogador conectado!");
+  socket.on("joinGame", () => { 
+    let roomJoined = null;
+
+    for (const roomId in rooms) {
+      const room = rooms[roomId];
+      if (room.players.length < 2) {
+        room.players.push(socket.id);
+        roomJoined = roomId;
+        break;
+      }
+    }
+
+    if (!roomJoined) {
+      const newRoomId = `room-${socket.id}`;
+      rooms[newRoomId] = { players: [socket.id], gameStarted: false };
+      roomJoined = newRoomId;
+    }
+
+    socket.join(roomJoined);
+    socket.emit("joinedRoom", roomJoined);
+
+    const room = rooms[roomJoined];
+    if (room.players.length === 2) {
+      room.gameStarted = true;
+      io.to(roomJoined).emit("startGame", { roomId: roomJoined });
+    }
+    
+  });
+  socket.on("placeShips", (ships) => { /* salvar posições */ });
+  socket.on("fire", (target) => { /* processar ataque */ });
 
   socket.on("disconnect", () => {
     console.log("Jogador desconectado.");
--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 // Conecta ao servidor via WebSocket
 const socket = io();
 
+socket.emit("joinGame"); 
+
 socket.on("connect", () => {
   console.log("Conectado ao servidor!");
 });
